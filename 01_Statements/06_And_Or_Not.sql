e3905ee3081d78133229083805d3d6affef0536d
ColladoWork <colladowork99@gmail.com>
2024-12-28 16:46:11 +0100

    Update 06_And_Or_Not.sql

--- 01_Statements/06_And_Or_Not.sql
+++ 01_Statements/06_And_Or_Not.sql
@@ -1,1 +1,8 @@
+-- Negacion de un criterio
+SELECT * FROM estrellas WHERE NOT tipo_de_estrella = 'M';
 
+-- Añadir varias sentencia de busqueda de cumplimiento obligatorio
+SELECT * FROM estrellas WHERE NOT tipo_de_estrella = 'M' AND planetas > 1;
+
+-- Añadir varias sentencia de busqueda donde multiples opciones son validas
+SELECT * FROM estrellas WHERE NOT tipo_de_estrella = 'M' OR planetas > 1;
