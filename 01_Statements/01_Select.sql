ac615f49964bc8a12a82f09c07cb1d7aea802999
ColladoWork <colladowork99@gmail.com>
2024-12-28 16:43:49 +0100

    Update 01_Select.sql

--- 01_Statements/01_Select.sql
+++ 01_Statements/01_Select.sql
@@ -1,1 +1,11 @@
+##############################
+## Herramienta de seleccion ##
+##############################
+
+-- 1) Seleccionar todos los elementos de una tabla
+SELECT * FROM ag_espaciales;
+
+-- 2) Seleccionar uno o varios elementos de una tabla
+SELECT sede_central FROM ag_espaciales;
+SELECT id_ag_esp, sede_central, presupuesto_anual FROM ag_espaciales;
 
