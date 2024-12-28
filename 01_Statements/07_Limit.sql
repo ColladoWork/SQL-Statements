daa088bce7d4d616e4fd5fe0deff9833f5aef82f
ColladoWork <colladowork99@gmail.com>
2024-12-28 16:46:32 +0100

    Update 07_Limit.sql

--- 01_Statements/07_Limit.sql
+++ 01_Statements/07_Limit.sql
@@ -1,1 +1,10 @@
+-- Sentencia LIMIT devuelve solo los n primeros elementos de la tabla.
+SELECT * FROM estrellas LIMIT 3;
 
+##############################################################################################
+## En bases de datos de cientos de miles o incluso millones de datos almacenados, esta      ##
+## secuencia es muy util para pulir llamadas a la base de datos como también, para trabajar ##
+## de forma mas eficiente.                                                                  ##
+##############################################################################################
+
+SELECT * FROM estrellas WHERE NOT tipo_de_estrella = 'M' LIMIT 4;
