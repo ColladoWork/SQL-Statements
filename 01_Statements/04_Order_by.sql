36cb22382039b718db9a8b7c061906a60aada227
ColladoWork <colladowork99@gmail.com>
2024-12-28 16:45:30 +0100

    Update 04_Order_by.sql

--- 01_Statements/04_Order_by.sql
+++ 01_Statements/04_Order_by.sql
@@ -1,1 +1,7 @@
+-- Ordenar los datos de manera ASCENDENTE.
+SELECT * FROM estrellas ORDER BY planetas;
 
+-- Ordenar los datos de manera DESCENDENTE.
+SELECT * FROM estrellas ORDER BY planetas DESC;
+
+SELECT * FROM estrellas WHERE distancia < 100 ORDER BY planetas DESC;
