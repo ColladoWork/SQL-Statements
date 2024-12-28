62696aea044888de92b055908d38675fdc3ac1d4
ColladoWork <colladowork99@gmail.com>
2024-12-28 16:45:13 +0100

    Update 03_Where.sql

--- 01_Statements/03_Where.sql
+++ 01_Statements/03_Where.sql
@@ -1,1 +1,8 @@
+##############################################################
+## Comando WHERE permite reducir los criterios de busqueda. ##
+## Indicamos una condicion con la que iniciar la consulta.  ##
+##############################################################
 
+SELECT * FROM estrellas WHERE tipo_de_estrella = 'M';
+
+SELECT * FROM estrellas WHERE masa > 1;
