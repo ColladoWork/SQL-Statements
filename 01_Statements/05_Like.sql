c58fcb4834aa741f2f6b26cb6e4714e838e71be1
ColladoWork <colladowork99@gmail.com>
2025-01-05 12:47:59 +0100

    Update 05_Like.sql

--- 01_Statements/05_Like.sql
+++ 01_Statements/05_Like.sql
@@ -1,11 +1,13 @@
 -- Utilizado para buscar patrones especificos en una columna.
 -- Es utilizado junto a la sentencia WHERE para buscar sentencias especificas
 
+/*
 ###################################################################################################
 ## Ejemplo: En el caso de las estrellas hay varias pertenecientes a la Constelacion de Acuario.  ##
 ## Con el comando LIKE podremos buscar en la columna de constelaciones en funcion de este valor. ##
 ## Es necesario que se use el caracter % para realizar la busqueda de tipado dinamico.           ##
 ###################################################################################################
+*/
 
 SELECT * FROM estrellas WHERE ubicacion LIKE '%Acuario';
 -- Un % al principio indicará que da igual todo lo que haya antes de esa linea de texto. Es la palabra con la que acaba la linea de texto.
