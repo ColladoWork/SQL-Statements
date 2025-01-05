495372380b2c126f34da8fd4c02a2e23ae25c4e7
ColladoWork <colladowork99@gmail.com>
2025-01-05 12:48:21 +0100

    Update 06_And_Or_Not.sql

--- 01_Statements/06_And_Or_Not.sql
+++ 01_Statements/06_And_Or_Not.sql
@@ -6,3 +6,7 @@ SELECT * FROM estrellas WHERE NOT tipo_de_estrella = 'M' AND planetas > 1;
 
 -- Añadir varias sentencia de busqueda donde multiples opciones son validas
 SELECT * FROM estrellas WHERE NOT tipo_de_estrella = 'M' OR planetas > 1;
+
+-- Otros ejemplos
+SELECT * from exoplanetas WHERE descubrimiento < 2018 AND zona_habitable = 'Si';
+SELECT * FROM catalogo WHERE tipo = 'Estrella' OR tipo = 'Exoplaneta';
