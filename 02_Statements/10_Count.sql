468ef6fe277639d2cb19c50daf4b5cedf62e6f5d
ColladoWork <colladowork99@gmail.com>
2025-01-06 12:53:14 +0100

    Create 10_Count.sql

---
+++ 02_Statements/10_Count.sql
@@ -0,0 +1,9 @@
+/*
+COUNT se encarga de contar cuantos elemetos de un atributo NO ESTAN A NULL.
+*/
+
+SELECT COUNT(planetas) from estrellas;
+
+-- Cuenta cuantos elementos hay en total en la tabla registrados.
+
+SELECT COUNT(*) from estrellas;
